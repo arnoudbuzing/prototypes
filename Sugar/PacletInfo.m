f758efa9f28a31e7c1fc63c221554ece4bedb39b
Arnoud Buzing <arnoudb@wolfram.com>
2017-12-01 08:51:29 -0600

    updates

--- Sugar/PacletInfo.m
+++ Sugar/PacletInfo.m
@@ -3,7 +3,7 @@ Paclet[
   Description -> "A paclet for miscellaneous functions",
   Creator -> "Arnoud Buzing",
   Publisher -> "Wolfram Research",
-  Version -> "0.0.1",
+  Version -> "0.0.2",
   MathematicaVersion -> "11.2+",
   Loading -> Automatic,
   Extensions -> {
