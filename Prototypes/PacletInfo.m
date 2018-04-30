e72375854f45b6943380a6bc917bf143fa6be289
Arnoud Buzing <arnoudb@wolfram.com>
2018-04-30 13:26:47 -0500

    updates

--- Prototypes/PacletInfo.m
+++ Prototypes/PacletInfo.m
@@ -3,7 +3,7 @@ Paclet[
   Description -> "A paclet for prototype functions",
   Creator -> "Arnoud Buzing",
   Publisher -> "Wolfram Research",
-  Version -> "0.2.2",
+  Version -> "0.2.3",
   MathematicaVersion -> "11.2+",
   Loading -> Automatic,
   Thumbnail -> "icons/icon.png",
